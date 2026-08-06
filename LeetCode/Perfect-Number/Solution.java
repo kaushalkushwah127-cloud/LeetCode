54b4897f2ac12b1b3d0522b079f3e1fd3af0963f
kaushalkushwah127-cloud <kaushalkushwah127@gmail.com>
2026-08-06 07:13:33 +0530

    Add solution for Perfect Number

--- LeetCode/Perfect-Number/Solution.java
+++ LeetCode/Perfect-Number/Solution.java
@@ -2,7 +2,7 @@
 2    public boolean checkPerfectNumber(int num) {
 3        if (num<2) return false;
 4        int sum=1;
-5        for (int i=2; i<num; i++){
+5        for (int i=2; i<=num/2; i++){
 6            if(num%i==0) sum+=i;
 7        }
 8        return num==sum;

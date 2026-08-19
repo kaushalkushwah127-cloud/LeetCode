a39fb571bb6d6d037a487ce5748c9a845867ea18
Kaushal_kushwaha <kaushalkushwah127@gmail.com>
2026-08-19 08:11:53 +0530

    Add solution for Length of Last Word

--- LeetCode/Length-of-Last-Word/Solution.java
+++ LeetCode/Length-of-Last-Word/Solution.java
@@ -1,6 +1,7 @@
 1class Solution {
 2    public int lengthOfLastWord(String s) {
 3    String [] ar=s.split("\\s+");
-4    return ar[ar.length-1].length();
-5    }
-6}
+4    String q=ar[ar.length-1];
+5    return q.length();
+6    }
+7}

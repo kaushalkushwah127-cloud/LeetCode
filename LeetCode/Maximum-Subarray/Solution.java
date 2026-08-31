401f8239415a763d339e6715c4287a9b47ae7815
Kaushal_kushwaha <kaushalkushwah127@gmail.com>
2026-08-31 13:38:46 +0530

    Add solution for Maximum Subarray

--- LeetCode/Maximum-Subarray/Solution.java
+++ LeetCode/Maximum-Subarray/Solution.java
@@ -1,11 +1,10 @@
 1class Solution {
 2    public int maxSubArray(int[] nums) {
-3        int max=Integer.MIN_VALUE,csum=0;
-4        for (int i=0; i<nums.length; i++){
-5            csum+=nums[i];
-6            max=Math.max(csum,max);
-7            if (csum<0) csum=0;
-8        }
-9        return max;
-10    }
-11}
+3        int sum = 0, max = nums[0];
+4        for (int n : nums) {
+5            sum = Math.max(n, sum + n);
+6            max = Math.max(max, sum);
+7        }
+8        return max;
+9    }
+10}

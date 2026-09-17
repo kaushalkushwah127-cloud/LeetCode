7c57c34839a32e01e4f858832e28431cc074ed68
Kaushal_kushwaha <kaushalkushwah127@gmail.com>
2026-09-17 07:46:03 +0530

    Add solution for Search Insert Position

--- LeetCode/Search-Insert-Position/Solution.java
+++ LeetCode/Search-Insert-Position/Solution.java
@@ -1,10 +1,20 @@
 1class Solution {
 2    public int searchInsert(int[] nums, int target) {
-3        ArrayList <Integer> list = new ArrayList <> ();
-4        for ( int i :nums) list.add(i);
-5        if (list.contains(target)) return list.indexOf(target);
-6        else list.add(target);
-7        list.sort(null);
-8        return list.indexOf(target);
-9    }
-10}
+3       int o=0; 
+4       for (int i=0; i<nums.length; i++){
+5        if (target==nums[i]){
+6            o=i;
+7            break;
+8        }
+9        else {
+10            if (target<nums[i]){
+11                o=i;
+12                break;
+13            }
+14            else o=nums.length;
+15        }
+16       }
+17       return o;
+18    
+19    }
+20}
